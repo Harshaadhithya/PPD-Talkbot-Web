4200d260371082b67dd8c2edec03a17f85fb39fe
Harshaadhithya <harshaadhithya1234@gmail.com>
2024-04-04 23:12:06 +0530

    url changes

--- users/urls.py
+++ users/urls.py
@@ -14,12 +14,7 @@ urlpatterns = [
 
     path('my_chats/', views.my_chats, name='my-chats'),
     path('new_chat', views.new_chat, name='new-chat'),
-    # path('send_user_message', views.)
 
     path('chat/<str:pk>/', views.chat, name='chat'),
 
-
-    # path('inbox/',views.inbox,name='inbox'),
-    # path('message/<str:pk>/',views.view_message,name='message'),
-    # path('send-message/<str:pk>/',views.send_message,name='send-message')
 ]
